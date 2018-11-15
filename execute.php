5cf4184772b3f1eb28103aa104f54a5f3748c2c7
Stefano Di Pasquale <stefaniscion@gmail.com>
2018-11-15 12:38:24 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -54,25 +54,25 @@ if(strtolower($username) == 'stefaniscion'){
   //HELP
 if($command == '/help' || $command == '/help@ilprocionebot'){
   $r_text = 
-'help - Imparara a comunicare con i procioni!
-about - Info su questo procione e sul suo creatore
-procioni - Invia foto di procioni
-selfie - Ti invio un mio selfie
-piave - Per soli veri procioni italiani';
+'**help** - Imparara a comunicare con i procioni!
+**about** - Info su questo procione e sul suo creatore
+**procioni** - Invia foto di procioni
+**selfie** - Ti invio un mio selfie
+**piave** - Per soli veri procioni italiani';
   $r_method  = "sendMessage";
 }
 
 //ABOUT
 if($command == '/about' || $command == '/about@ilprocionebot'){
   $r_text = 
-'Il Procione
+'**Il Procione**
 Il Procione è il bot definitivo per gli fan e haters dei procioni!
 
 Corretto e mantenuto da @Stefaniscion
 Consigli? Suggerimenti? Bug? Scrivi a @Stefaniscion
 Puoi trovare il procione sorgente su: https://github.com/Stefaniscion/ilprocione-bot
 
-Versione: v1.0 Honey Raccoon - 15/11/2018
+Versione: `v1.0 __Honey Raccoon__` - 15/11/2018
 -In memoria di Alex-';
   $r_method  = "sendMessage";
 }
