14da45b38899dfd265e0f9e85052e54b7c033f8b
Stefano Di Pasquale <stefaniscion@gmail.com>
2018-11-15 11:31:15 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
+<?php
+
 function send($response,$method,$chatId){
   header("Content-Type: application/json");
   $parameters = array('chat_id' => $chatId, "text" => $response);
   $parameters["method"] = $method;
   echo json_encode($parameters);
 }
+
+?>
